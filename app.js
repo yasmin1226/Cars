9225c7f0db7ede9659b676f0fa6d55e78d9990d4
yasmin <yasminfathy630@gmail.com>
2021-07-02 06:20:52 +0200

    login

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-
+app.set("view engine", "ejs");
 //middleware
 
 app.use(cookieParser());
@@ -10,13 +10,15 @@ app.use(express.json());
 
 //
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/views`));
 
 //routes
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/users", require("./routes/users"));
-app.get("/", (req, res) => {
-  res.send("heruko!");
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/table", (req, res) => {
+  res.render("table");
 });
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -6,36 +6,6 @@ const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
-const { userAuth, adminAuth } = require("../middlewares/auth");
-
-//@ route          api/auth/loadUser
-//@descrption      user
-//@access          private
-//get authenticated user  data upon login
-router.get("/loadUser", userAuth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select("-password");
-
-    res.status(200).json(user);
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).send("Server error");
-  }
-});
-
-//@ route          api/auth/loadAdmin
-//@descrption      admin
-//@access          private
-//get authenticated  admin data upon login
-router.get("/loadAdmin", adminAuth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select("-password");
-    res.status(200).json(user);
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).send("Server error");
-  }
-});
 
 //@ route          POST   api/auth
 //@descrption      authenticate and login user or admin & get his\her token
@@ -54,62 +24,20 @@ router.post(
 
     const { username, password } = req.body;
     try {
-      //see if user exist
-      let user = await User.findOne({ username });
-      if (!user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Invalid Credentials" }] });
-      }
-
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Invalid Credentials" }] });
-      }
-
-      //return JWT
-      const payload = {
-        user: {
-          id: user.id,
-          isAdmin: user.isAdmin,
-        },
-      };
-      jwt.sign(payload, "jwtSecret", { expiresIn: 36000 }, (err, token) => {
-        if (err) throw err;
-        if (user.isAdmin) {
-          res.status(200).json({
-            msg: "Admin logged in successfully",
-            token,
-            isAdmin: user.isAdmin,
-          });
-        }
+      if (username === "admin" && password === "admin") {
         res.status(200).json({
-          msg: "User logged in successfully",
-          token,
-          isAdmin: user.isAdmin,
+          msg: "Admin logged in successfully",
         });
-      });
+      } else {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Server error");
     }
   }
 );
-
-//get user by id -- authenication needed here ###
-router.get("/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send({ message: "User Not found." });
-    }
-    res.status(200).json(user);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send("Server error");
-  }
-});
 
 module.exports = router;
